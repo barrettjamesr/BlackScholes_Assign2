dfc3c9fa7c96671ae259a1f316790b6b08741e38
James R. Barrett, CFA <barrett.jamesr@gmail.com>
2017-03-23 14:14:52 +0800

    pandas mostly working

--- ImpliedVol.py
+++ ImpliedVol.py
@@ -15,59 +15,65 @@ def FullBlackScholesPut(S, X, r, t, vol, q):
     
     return norm.cdf(0-d2) * float(X) / np.exp(float(r) * float(t)) -float(S) / np.exp(float(q) * float(t)) * norm.cdf(0-d1)
 
-def ImpliedVolatility(S, X, r, t, vol, q):
+def ImpliedVolatilityCall(S, X, r, t, vol, q):
+    
+    
+    return 0
+
+def ImpliedVolatilityPut(S, X, r, t, vol, q):
     
     
     return 0
 
 
 def main():
+    #constants
+    r = 0.04
+    q = 0.20
+
     #read instrument data
-    
-    #f = open("C:\\Users\\James\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\instruments.csv", "r")
-    #reader = csv.reader(f)
-    #next(reader, None)
-    #securities = list(reader)
-    #f.close()
-    securities = pd.read_csv("C:\\Users\\guest2\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\instruments.csv")
-    securities.set_index('Symbol')
-    
+    securities = pd.read_csv("C:\\Users\\James\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\instruments.csv", dtype = str)
+    securities = securities.set_index('Symbol')
+    securities['Expiry'] = pd.to_datetime(securities['Expiry'], errors='coerce')
+
+    #securities['Expiry'] = securities['Expiry'].apply(lambda x: dt.datetime.strptime(str(x),'%Y%m%d'))
+    #print(securities)
+
     #read price data
-    #f = open("C:\\Users\\James\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\marketdata.csv", "r")
-    #reader = csv.reader(f)
-    #next(reader, None)
-    #market_data = list(reader)
-    #f.close()
-    market_data = pd.read_csv("C:\\Users\\guest2\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\marketdata.csv")
-    market_data.set_index('LocalTime')
-    market_data['LocalTime'] = market_data['LocalTime'].apply(lambda x: 
-                                    dt.datetime.strptime(x,'%Y-%b-%d %I:%M:%S.%f'))
+    market_data = pd.read_csv("C:\\Users\\James\\SkyDrive\\Documents\\HKU\\TechniquesInCompFin\\marketdata.csv", dtype = str)
+    market_data['LocalTime'] = pd.to_datetime(market_data['LocalTime'], errors='coerce')
+    #market_data['LocalTime'] = market_data['LocalTime'].apply(lambda x: dt.datetime.strptime(x,'%Y-%b-%d %I:%M:%S.%f'))
     market_data = market_data.sort_values('LocalTime')
 
+    # process data into 31, 32 & 33
     data31 = market_data[(market_data['LocalTime'] < dt.datetime(2016, 2, 16, 9, 31, 0, 0))]
     data32 = market_data[(market_data['LocalTime'] < dt.datetime(2016, 2, 16, 9, 32, 0, 0))]
     data33 = market_data[(market_data['LocalTime'] < dt.datetime(2016, 2, 16, 9, 33, 0, 0))]
 
-    #data31 = data31.groupby("Symbol").apply(lambda d:d.loc[d.LocalTime.idxmax()])
-
-    data31 = data31[['LocalTime', 'Symbol', 'Last', 'Bid1', 'Ask1']].groupby('Symbol').last()
-
-    combined31 = pd.concat([securities, data31.reset_index()], axis=1)
-    
-
-    #print(securities)
-    #print(market_data)
-    # process data into 31, 32 & 33
-
-    #print(data31.reset_index())
-    print(combined31)
-
     # get most recent price for the minute
+    data31 = data31.iloc[data31.groupby('Symbol')['LocalTime'].idxmax().values.ravel()]
+    data32 = data32.iloc[data31.groupby('Symbol')['LocalTime'].idxmax().values.ravel()]
+    data33 = data33.iloc[data31.groupby('Symbol')['LocalTime'].idxmax().values.ravel()]
 
     # join with instrument details
+    data31 = data31.join(securities, on='Symbol')
+    data32 = data32.join(securities, on='Symbol')
+    data33 = data33.join(securities, on='Symbol')
 
+    #separate into options and etf
+    a50etf31 = data31[(data31['Type'] == 'Equity')].reset_index(drop=True) 
+    a50etf32 = data32[(data32['Type'] == 'Equity')].reset_index(drop=True) 
+    a50etf33 = data33[(data33['Type'] == 'Equity')].reset_index(drop=True) 
+
+    data31 = data31[(data31['Type'] == 'Option')].reset_index(drop=True) 
+    data32 = data32[(data32['Type'] == 'Option')].reset_index(drop=True) 
+    data33 = data33[(data33['Type'] == 'Option')].reset_index(drop=True) 
     
     #calculate implied vol
+
+    for index, row in data31.iterrows():
+        if 'C' == row['OptionType']:
+            ImpVolCallBid = ImpliedVolatilityCall(a50etf31['Bid1'], row['Strike'], r,  data31['LocalTime'], data31['Bid1'],q)
 
 
     #save results to \31.csv", \32.csv", and \33.csv"
